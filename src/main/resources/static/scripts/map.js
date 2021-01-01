f1ffee664114fb56a64bb878facdfc6b59597887
PavloDrabchuk <ravluk2000@gmail.com>
2021-01-01 22:01:49 +0200

    update map view by one

---
+++ src/main/resources/static/scripts/map.js
@@ -0,0 +1,40 @@
+
+let map = new OpenLayers.Map("mapBlock", {
+    controls: [
+        new OpenLayers.Control.Navigation(),
+        new OpenLayers.Control.PanZoomBar(),
+        new OpenLayers.Control.LayerSwitcher(),
+        new OpenLayers.Control.Attribution()],
+    //maxExtent: new OpenLayers.Bounds(-20037508.34,-20037508.34,20037508.34,20037508.34),
+    //maxResolution: 156543.0399,
+    numZoomLevels: 19,
+    //units: 'm',
+    projection: new OpenLayers.Projection("EPSG:900913"),
+    displayProjection: new OpenLayers.Projection("EPSG:4326")
+});
+
+// Define the map layer
+// Here we use a predefined layer that will be kept up to date with URL changes
+layerMapnik = new OpenLayers.Layer.OSM.Mapnik("Mapnik");
+map.addLayer(layerMapnik);
+layerCycleMap = new OpenLayers.Layer.OSM.CycleMap("CycleMap");
+map.addLayer(layerCycleMap);
+layerMarkers = new OpenLayers.Layer.Markers("Markers");
+map.addLayer(layerMarkers);
+
+
+// Start position for the map (hardcoded here for simplicity,
+// but maybe you want to get this from the URL params)
+
+
+const zoom = 12;
+let lonLat = new OpenLayers.LonLat(lon, lat).transform(new OpenLayers.Projection("EPSG:4326"), map.getProjectionObject());
+
+// Add a Layer with Marker
+let size = new OpenLayers.Size(21, 25);
+let offset = new OpenLayers.Pixel(-(size.w / 2), -size.h);
+let icon = new OpenLayers.Icon('https://www.openstreetmap.org/openlayers/img/marker.png', size, offset);
+layerMarkers.addMarker(new OpenLayers.Marker(lonLat, icon));
+
+map.setCenter(lonLat, zoom);
+
