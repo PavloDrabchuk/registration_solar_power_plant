89e4b06163a9f6c06c1fde35eb7ceb9255b01821
PavloDrabchuk <ravluk2000@gmail.com>
2021-02-06 19:03:23 +0200

    update generate producer power function

--- src/main/java/com/example/demo/model/Weather.java
+++ src/main/java/com/example/demo/model/Weather.java
@@ -2,14 +2,14 @@ package com.example.demo.model;
 
 public enum Weather {
     ClearSky(1D),
-    FewClouds(0.8D),
-    ScatteredClouds(0.5D),
-    BrokenClouds(0.3D),
+    FewClouds(0.8D), //60-80%
+    ScatteredClouds(0.4D), //30-40%
+    BrokenClouds(0.3D), //20-30%
     ShowerRain(0.1D),
     Rain(0.2D),
     Thunderstorm(0.01D),
-    Snow(0.4D),
-    Mist(0.05D);
+    Mist(0.05D),
+    Snow(0.4D);
 
     private final double coefficient;
 
--- src/main/java/com/example/demo/service/DynamicDataService.java
+++ src/main/java/com/example/demo/service/DynamicDataService.java
@@ -3,12 +3,14 @@ package com.example.demo.service;
 import com.example.demo.dao.DynamicDataRepository;
 import com.example.demo.model.DynamicData;
 import com.example.demo.model.SolarPowerPlant;
+import com.example.demo.model.Weather;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
@@ -32,32 +34,65 @@ public class DynamicDataService {
                 leftLimit = 1D,
                 rightLimit = 100D;
 
+        LocalDateTime localDateTime = LocalDateTime.now();
+
         for (SolarPowerPlant solarPowerPlant : solarPowerPlantList) {
             // producedPower = leftLimit + new Random().nextDouble() * (rightLimit - leftLimit);
+            System.out.println("Month: " + localDateTime.getMonthValue());
 
-            producedPower=generateProducerPower();
+            producedPower = generateProducerPower(localDateTime.getMonthValue());
 
-            generatedData.add(new DynamicData(solarPowerPlant, producedPower, LocalDateTime.now()));
+            generatedData.add(new DynamicData(solarPowerPlant, producedPower, localDateTime));
         }
 
         return generatedData;
     }
 
     @Async
-    @Scheduled(fixedRate = 5*60*1000)
+    @Scheduled(fixedRate = 8 * 1000/*2 * 60 * 1000*/)
     public void saveDynamicData() {
         System.out.println("\n\n save dynamic data: ");
         //dynamicDataRepository.save(dynamicData);
         for (DynamicData dynamicData : generateData()) {
-            System.out.println("  id: "+dynamicData.getSolarPowerPlant().getId());
+            System.out.println("  id: " + dynamicData.getSolarPowerPlant().getId());
             dynamicDataRepository.save(dynamicData);
         }
+        System.out.println("\n\n\n");
     }
 
-    private double generateProducerPower(){
-        double producerPower=0;
+    private double generateProducerPower(int month) {
+        double producerPower = 340;
+        producerPower *= monthlyCoefficient(month) * weatherCoefficient(month);
+
+        /*
+        Months.values()[index]
+         */
+
+
+        //ArrayList<Weather> weathers = new ArrayList<Weather>();
+        //System.out.println("Arrays: "+Arrays.toString(Weather.values()));
+        System.out.println("Produced power: " + producerPower);
+
+        System.out.println("Monthly coefficient: " + monthlyCoefficient(month));
+
+        System.out.println("Weather coefficient: " + weatherCoefficient(month));
 
         return producerPower;
+    }
+
+    private double monthlyCoefficient(int month) {
+        return ((Math.sin(0.4 * month - 1.02)) + 1) / 2;
+    }
+
+    private double weatherCoefficient(int month) {
+        int weatherNumber;
+        do {
+            weatherNumber = new Random().nextInt(Weather.values().length);
+            //System.out.println("weatherNumber: "+weatherNumber+"  name: "+Weather.values()[weatherNumber].name());
+        } while (month > 3 && month < 10 && Weather.values()[weatherNumber].name().equals("Snow"));
+
+        //System.out.println("w n: " + weatherNumber);
+        return Weather.values()[weatherNumber].getCoefficient();
     }
 }
 
